517467e598c6298f201a8ee82f150b0293eb82f9
sanjanayadav077 <sanjana.28472@stu.upes.ac.in>
2026-02-22 23:09:09 +0530

    Add files via upload

---
+++ Days_09.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void reverseString(char* s, int size) {
+    int left = 0, right = size - 1;
+    while (left < right) {
+        char temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+int main() {
+    char s[] = {'h','e','l','l','o'};
+    int size = sizeof(s) / sizeof(s[0]);
+
+    reverseString(s, size);
+
+    for (int i = 0; i < size; i++) {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
