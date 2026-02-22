56e8270c0d74ef4c301afd28278b7f12a01c898b
sanjanayadav077 <sanjana.28472@stu.upes.ac.in>
2026-02-22 23:21:02 +0530

    Add files via upload

---
+++ Days_14.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+void rotate(int** matrix, int matrixSize, int* matrixColSize) {
+    int n = matrixSize;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int temp = matrix[i][left];
+            matrix[i][left] = matrix[i][right];
+            matrix[i][right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+int main() {
+    int n = 3;
+    int row0[] = {1, 2, 3};
+    int row1[] = {4, 5, 6};
+    int row2[] = {7, 8, 9};
+    int* matrix[] = {row0, row1, row2};
+    int colSize[] = {n, n, n};
+    rotate(matrix, n, colSize);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
